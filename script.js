a3ff9b22f06e582afd6d4b8156b947003ed62ae8
pachecur <45081469+pachecur@users.noreply.github.com>
2018-11-15 18:53:28 -0600

    agregando archibos

---
+++ script.js
@@ -0,0 +1,24 @@
+var CBI1 = 0;
+var CBI2 = 0;
+var CBD1 = 0;
+var CBD2 = 0;
+var ladoBarca = 'derecha';
+
+
+function subir(id){
+	if(ladoBarca == 'derecha'){
+		if(id[0] == 'C'){
+			if(id[1] == 'D'){
+
+			}else{
+				alert('No se puede subir porque la barca esta de el otro lado')
+			}
+		}else{
+
+		}
+		
+		
+	}
+	
+
+}
